2a66b126eb991ecf8776415959b03ccf20f68c8e
angie <angiemmar06@gmail.com>
2026-04-01 16:54:15 +0200

    creacion static

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -190,6 +190,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static_root/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
