4e4420b769eacaa9f3e96b3f694b0b729c2fad48
angiemmar06-cell <angiemmar06@gmail.com>
2026-05-31 17:01:13 +0200

    Registrar modelos de hidrografia en el admin

--- djangoapi/hidrografia_django/admin.py
+++ djangoapi/hidrografia_django/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cauces, EstacionesMonitoreo, Subcuencas
 
 # Register your models here.
+admin.site.register(Cauces)
+admin.site.register(EstacionesMonitoreo)
+admin.site.register(Subcuencas)
